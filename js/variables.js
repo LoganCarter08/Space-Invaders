714af0d9c9824e43b526238dfa3b6ad1caa4e66a
Logan <loganvt8@vt.edu>
2018-08-18 03:53:24 +0000

    Upload New File

---
+++ js/variables.js
@@ -0,0 +1,18 @@
+level = 1;
+gs=tc=20;
+over = 0;
+moveDown = 0;
+deadCount = 0;
+mothershipX = 1;
+bigCounter = 0;
+bulletCounter = 100;
+playerCounter = 0;
+playerLifeTaken = 0;
+wait = 0;
+bombDelayConst = 500 - (level * 20);
+bombDelay = bombDelayConst;
+lives = 3;
+score = 0;
+levelUpDelay = 0;
+startDelay = 750;
+
